679ef153ee35fabd2c1cf25c537a5bd5f556dedb
Mister1SI <wazzup2690@gmail.com>
2024-03-13 14:49:58 +0000

    Process arguments

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
-int main() {
-	puts("Hello");
+void help();
+
+int main(int argc, char** argv) {
+	int edit_mode;
+	char* filename;
+	char field;
+	switch(argc) {
+		case 1:
+			help();
+			return 0;
+		case 2:
+			edit_mode = 0;
+			filename = argv[1];
+			break;
+		case 3:
+			edit_mode = 1;
+			filename = argv[1];
+			field = argv[2][0];
+			break;
+		default:
+			puts("Incorrect number of arguments");
+			help();
+			return 1;
+	}
+	
 	return 0;
 }
+
+void help() {
+	puts(
+	"Help menu"
+	);
+}
+
